3738744dee92396f590568c3b97e7bdaefbccafb
lukasbogota <49100627+lukasbogota@users.noreply.github.com>
2019-04-08 15:12:07 -0500

    Add files via upload

--- PG2Pyomo(ArcsInRoute).py
+++ PG2Pyomo(ArcsInRoute).py
@@ -17,9 +17,10 @@ model = pyo.AbstractModel()
                   
 model.NOD = pyo.Set()                       # Conjunto de Nodos de Origen y Destino
 model.NEst = pyo.Set()                      # Conjunto de Nodos de estaciones
+model.NSink = pyo.Set() 
 
 #model.N = pyo.Set()
-model.N = model.NOD | model.NEst        # Conjunto de Nodos, unión de nodos OD y estaciones
+model.N = model.NOD | model.NEst | model.NSink       # Conjunto de Nodos, unión de nodos OD y estaciones
 
 
 model.A = pyo.Set(within=model.N*model.N)              # Conjunto de Arcos, definidos en los parametros
@@ -49,10 +50,10 @@ model.NodesIn = pyo.Set(model.N, initialize=NodesIn_init)
 model.NEstIn = pyo.Set(model.NEst, initialize=NodesIn_init)
 
 #Define el subconjunto de arcos que componen una ruta
-def ArcsInRoute_init(model, no, nd): 
+def ArcsInOD_init(model, no, nd): 
     retval = []
     for ni in model.NodesOut[no]:
-        if ni == "E0":
+        if ni in model.NSink: 
             if (ni,nd) in model.A:
                 retval.append((no,ni))
                 retval.append((ni,nd))
@@ -65,7 +66,7 @@ def ArcsInRoute_init(model, no, nd):
                           retval.append((ni,nj))     
                           retval.append((nj,nd))                     
     return retval
-model.ArcsInRoute = pyo.Set(model.OD,within=model.N*model.N, initialize=ArcsInRoute_init)
+model.ArcsInOD = pyo.Set(model.OD,within=model.N*model.N, initialize=ArcsInOD_init)
 
 
 model.Tmax = pyo.Param()
@@ -85,9 +86,9 @@ model.Cmantenimiento = pyo.Param()                          #Costo diario de ten
 model.Cadquisicion = pyo.Param()                            #Costo de adquirir una bicicleta
 
 
-#Defino las variables de desición 
+#Defino las variables de decisión 
 
-# model.x = pyo.Var(model.OD, model.ArcsInRoute[model.OD], model.T, within=pyo.NonNegativeReals)                 #Flujo del arco (ni,nj)  para pasajeros con origen no  y destino nd,en la franja de tiempo t. ∀ ni,nj,no,nd ∈ N, t ∈ T.
+#model.x = pyo.Var(model.OD, model.ArcsInOD, model.T, within=pyo.NonNegativeReals)                 #Flujo del arco (ni,nj)  para pasajeros con origen no  y destino nd,en la franja de tiempo t. ∀ ni,nj,no,nd ∈ N, t ∈ T.
 model.x = pyo.Var(model.OD, model.A, model.T, within=pyo.NonNegativeReals)  
 model.y = pyo.Var(model.NEst, model.W, within=pyo.Binary)                                         # 1: si en el nodo ni  ∈ CN(est)  se decide construir una estación de tamaño w ∈ W. 0 dlc
 model.z = pyo.Var(model.NEst,model.T, within=pyo.NonNegativeReals)                              # Inventario en la estación ni  al inicio de la franja de tiempo t.  ∀  ni ∈ NC(est),t ∈ T  
@@ -99,7 +100,7 @@ model.z = pyo.Var(model.NEst,model.T, within=pyo.NonNegativeReals)              
 #minimiza el costo de uso y de operación del sistema
 
 def obj_rule(model):
-    return (sum(model.x[od,a,t]*model.c[a] for od in model.OD for a in model.ArcsInRoute[od] for t in model.T  )+sum (model.z[ni,1]*model.Cmantenimiento for ni in model.NEst)) #Preguntar notación t=1
+    return (sum(model.x[od,a,t]*model.c[a] for od in model.OD for a in model.ArcsInOD[od] for t in model.T  )+sum (model.z[ni,1]*model.Cmantenimiento for ni in model.NEst)) #Preguntar notación t=1
 model.obj = pyo.Objective(rule=obj_rule)
 #Preguntar recorridos sobre arcos
 
@@ -107,45 +108,33 @@ model.obj = pyo.Objective(rule=obj_rule)
 
 # 1. Debo cumplir con todas las demandas entre puntos de origen y destino:
 def cumplir_demandas_rule(model,no,nd,ni,t):
-    return ((sum(model.x[no,nd,ni,nj,t] for nj in model.NodesOut[ni] if (ni,nj) in model.ArcsInRoute[no,nd])  - sum(model.x[no,nd,nj,ni,t] for nj in model.NodesIn[ni] if (ni,nj) in model.ArcsInRoute[no,nd]))==model.b[no,nd,ni,t])
+    return ((sum(model.x[no,nd,ni,nj,t] for nj in model.NodesOut[ni] if (ni,nj) in model.ArcsInOD[no,nd])  - sum(model.x[no,nd,nj,ni,t] for nj in model.NodesIn[ni] if (ni,nj) in model.ArcsInOD[no,nd]))==model.b[no,nd,ni,t])
 model.cumplir_demandas_rule = pyo.Constraint(model.OD, model.N, model.T, rule= cumplir_demandas_rule)
 
 
 #2. En caso de que decida abrir una estación sólo le puedo asignar un tamaño
 def un_tamaño_por_estacion_rule(model, ni):
-        if ni == "E0":
-            return sum(model.y[ni,w] for w in model.W) == 0
-        else:
-            return sum(model.y[ni,w] for w in model.W) <= 1  
+    return sum(model.y[ni,w] for w in model.W) <= 1  
 model.one_size_per_station_rule = pyo.Constraint(model.NEst, rule=un_tamaño_por_estacion_rule)
 
 
 
 #3. Balanceo de inventario de bicicletas
 def balanceo_bicicletas_rule(model,ni,t):
-    if ni == "E0":
-        return (model.z[ni,t]==0)
+    if t < model.Tmax:
+        return ((sum(model.x[od,nj,ni,t] for od in model.OD for nj in model.NEstIn[ni]if (ni,nj) in model.ArcsInOD[od] ) - sum(model.x[od,ni,nj,t] for od in model.OD for nj in model.NEstOut[ni] if (ni,nj) in model.ArcsInOD[od] ) + model.z[ni,t])== model.z[ni,(t+1)])
     else:
-        if t < model.Tmax:
-            return ((sum(model.x[od,nj,ni,t] for od in model.OD for nj in model.NEstIn[ni]if (ni,nj) in model.ArcsInRoute[od] ) - sum(model.x[od,ni,nj,t] for od in model.OD for nj in model.NEstOut[ni] if (ni,nj) in model.ArcsInRoute[od] ) + model.z[ni,t])== model.z[ni,(t+1)])
-        else:
-            return pyo.Constraint.Skip
-model.balanceo_bicicletas_rule = pyo.Constraint(model.NEst, model.T, rule=balanceo_bicicletas_rule) #PREGUNTAR!!
+        return pyo.Constraint.Skip
+model.balanceo_bicicletas_rule = pyo.Constraint(model.NEst, model.T, rule=balanceo_bicicletas_rule)
 
 #4. Para cada estación, el inventario no puede superar la capacidad en ninguna franja horaria
 def capcidad_inventario_rule(model,ni,t):
-    if ni == "E0":
-         return pyo.Constraint.Skip
-    else:
-        return (model.z[ni,t]<=sum(model.y[ni,w]*model.capacidad[w] for w in model.W))
+    return (model.z[ni,t]<=sum(model.y[ni,w]*model.capacidad[w] for w in model.W))
 model.capcidad_inventario_rule = pyo.Constraint(model.NEst, model.T, rule=capcidad_inventario_rule)
 
 #5.Para cada estación, el flujo de salida de una estación hacia una estación no puede superar a la flota de bicicletas disponible en ese momento
 def flujo_salida_inventario_rule(model,ni,t):
-     if ni == "E0":
-         return pyo.Constraint.Skip
-     else:
-         return (sum(model.x[od,ni,nj,t] for od in model.OD for nj in model.NEstOut if (ni,nj) in model.ArcsInRoute[od]) <= model.z[ni,t])
+    return (sum(model.x[od,ni,nj,t] for od in model.OD for nj in model.NEstOut if (ni,nj) in model.ArcsInOD[od]) <= model.z[ni,t])
 model.flujo_salida_inventario_rule = pyo.Constraint(model.NEst, model.T, rule= flujo_salida_inventario_rule)
 
 #9.Presupuesto
